2fe60ce2e87e2d848308c830f306560d7e46ea67
Ella <ellalearnscopy@gmail.com>
2022-10-28 15:48:38 +0100

    sixth commit

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,19 +1,8 @@
 from flask import Flask, request, abort, jsonify
-from flask_cors import CORS
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
-    CORS(app, origins='*')
-
-    @app.after_request
-    def after_request(response):
-        response.headers.add("Access-Control-Allow-Headers", "Content-Type")
-        response.headers.add("Access-Control-Allow-Headers", "Authorization")
-        response.headers.add("Access-Control-Allow-Headers", "true")
-        response.headers.add("Access-Control-Allow-Methods", "GET, OPTIONS")
-        response.headers.add("Access-Control-Allow-Origin", "*")
-        return response
 
     @app.route('/', methods=['GET'])
     def get_details():
